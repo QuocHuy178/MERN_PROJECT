b3dec7a9c2ae63d9cff479a2c2a9872676ae8600
LAPTOP-9F4GMIKT\ASUS <huyblink123@gmail.com>
2022-04-01 15:55:29 +0700

    4:42

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,30 @@
 import React from "react";
-import {BrowserRouter as Router} from 'react-router-dom'
-import {DataProvider} from './GlobalState'
-import Header from './components/layouts/Header'
-import Sidebar from './components/layouts/Sidebar'
-import Slider from './components/layouts/Slider'
-import Footer from './components/layouts/Footer'
+import { BrowserRouter as Router } from "react-router-dom";
+import { DataProvider } from "./GlobalState";
+import Header from "./components/layouts/Header";
+import Sidebar from "./components/layouts/Sidebar";
+import Slider from "./components/layouts/Slider";
 
-import MainPages from './components/mainpages/Pages'
 
+import MainPages from "./components/mainpages/Pages";
 
 function App() {
   return (
     <DataProvider>
       <Router>
         <div className="App">
-         <Header/>
-         <Sidebar/>
-         <MainPages/>
+          <Header />
+          <div id="mainBody">
+            <div className="container">
+              <div className="row">
+                <Sidebar />
+                <div className="span9">
+                <MainPages />
+                </div>
+               
+              </div>
+            </div>
+          </div>
         </div>
       </Router>
     </DataProvider>
@@ -24,5 +32,3 @@ function App() {
 }
 
 export default App;
-
-
